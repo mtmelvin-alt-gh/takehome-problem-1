8f83f85e53fcb74a048d724d57aae3d226aa5f97
Murray <murray.melvin@gmail.com>
2024-04-05 18:03:42 -0500

    added some simple comments

--- src/main/scala/BestPriceFinder.scala
+++ src/main/scala/BestPriceFinder.scala
@@ -2,12 +2,16 @@ import scala.collection.mutable.ListBuffer
 
 case class BestPriceFinder() {
   def getBestGroupPrices(rates: Seq[Rate], prices: Seq[CabinPrice]): Seq[BestGroupPrice] = {
+    // Associate all the rates with prices for easier usage later
     val joined = for {
       rate <- rates
       price <- prices if rate.rateCode == price.rateCode
     } yield (rate, price)
 
+    // Sort them by price and rate group so it's easier to quickly find the lowest price per rate group
     val sorted = joined.sortBy(r => (r._2.price, r._1.rateGroup))
+
+    // And now put that into a Seq[CabinPriceRate] so we don't have to fool with tuples (for readability)
     val cabinPriceRates = sorted.map { case (rate, price) => CabinPriceRate(rate, price) }
 
     val rateGroups = rates.distinctBy(r => r.rateGroup)
